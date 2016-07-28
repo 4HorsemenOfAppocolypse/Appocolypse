e43b5e47ecbf886e3ddc2986bf210d92d34286f5
Marcus Joyner <marcus@seattlemarcus.com>
2016-07-27 18:03:32 -0700

    Initial Commit
    This is the empty template.

---
+++ RSSAppocolypse/app/src/main/java/itc298final/com/rssappocolypse/MainMenuActivity.java
@@ -0,0 +1,24 @@
+package itc298final.com.rssappocolypse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainMenuActivity extends Activity {
+
+    private ImageButton localImageButton;
+    private ImageButton nationalImageButton;
+    private ImageButton regionalImageButton;
+    private ImageButton worldImageButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+
+
+    }
+}
